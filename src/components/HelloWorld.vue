2ef341113367f20db7ddf3b9f199134e283a66fa
abahrisd <abakhri.sd@gmail.com>
2019-03-13 09:34:19 +0300

    looked at lifecycle hooks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,68 @@
 <template>
-    <div>
-        <span>HelloWorld!</span>
+    <div class="life-cycle-hooks">
+        <div>
+            <span>HelloWorld!</span>
+        </div>
+        <div>
+            <span>{{property}}</span>
+        </div>
+        <div class="life-cycle-hooks__counter">
+            <span>{{count}}</span>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: 'HelloWorld',
+        data() {
+            return {
+                property: 'Blank',
+                count: 0,
+            };
+        },
+        beforeCreate() {
+            console.log('Hello world: beforeCreate');
+        },
+        beforeUpdate() {
+            // this.property = `Count: ${this.count}`;
+            console.log('Hello world: beforeUpdate');
+        },
+        created() {
+            this.property = 'Example property update.';
+            console.log('Hello world: created');
+
+            setInterval(() => {
+                this.count += 1;
+            }, 1000);
+        },
+        updated() {
+            this.property = `Count: ${this.count}`;
+            console.log('Hello world: updated');
+        },
+        // not for requests
+        beforeMount() {
+            console.log('Hello world: beforeMount');
+        },
+        mounted() {
+            console.log('Hello world: mounted');
+        },
+        beforeDestroy() {
+            console.log('Hello world: beforeDestroy');
+        },
+        destroyed() {
+            console.log('Hello world: destroyed');
+        },
     };
 </script>
 
 <style scoped>
+    .life-cycle-hooks {
+        margin-left: 20px;
+    }
 
+    .life-cycle-hooks__counter {
+        font-size: 18px;
+        line-height: 20px;
+    }
 </style>
